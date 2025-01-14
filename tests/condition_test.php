75306f9b3011d6efdf60290729e5141c07498a11
valentinatessarounitn <valentina.tessaro.unitn@outlook.it>
2025-01-14 12:17:50 +0000

    Add tests

--- tests/condition_test.php
+++ tests/condition_test.php
@@ -14,6 +14,12 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
+use core_availability\info;
+use core_availability\info_module;
+use core_availability\info_section;
+use availability_recompletion\condition;
+use PHPUnit\Framework\TestCase;
+
 /**
  * Unit tests for the recompletion condition.
  *
@@ -21,33 +27,102 @@
  * @copyright   2024 Tessaro Valentina <valentina.tessaro@unitn.it>>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
- namespace availability_recompletion;
-
- use advanced_testcase;
- use core_availability\{tree, mock_info, info_module, info_section};
- use stdClass;
- use availability_recompletion\condition;
-
- /**
- * Unit tests for the recompletion condition.
- *
- * @package availability_recompletion
- * @copyright   2024 Tessaro Valentina <valentina.tessaro@unitn.it>>
- * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-
 class condition_test extends advanced_testcase {
 
     /**
+     * Setup to ensure that fixtures are loaded.
+     */
+    public function setUp(): void {
+        global $CFG;
+        parent::setUp();
+
+        // Load the mock info class so that it can be used.
+        require_once($CFG->dirroot . '/availability/tests/fixtures/mock_info.php');
+        require_once($CFG->dirroot . '/availability/tests/fixtures/mock_info_module.php');
+        require_once($CFG->dirroot . '/availability/tests/fixtures/mock_info_section.php');
+        require_once($CFG->libdir . '/completionlib.php');
+        $this->resetAfterTest();
+        $this->setAdminUser();
+        $CFG->enablecompletion = true;
+        $CFG->enableavailability = true;
+        set_config('enableavailability', true);
+        $dg = $this->getDataGenerator();
+        $now = time();
+        $this->course = $dg->create_course(['startdate' => $now, 'enddate' => $now + 7 * WEEKSECS, 'enablecompletion' => 1]);
+        $this->user = $dg->create_user(['timezone' => 'UTC']);
+        $dg->enrol_user($this->user->id, $this->course->id, 5, time());
+    }
+
+    /**
      * Tests the save() function.
-     * 
+     *
      * @covers \availability_recompletion\condition::save()
      */
-    public function test_save() {
+    public function test_save_equals() {
         $structure = (object)['cm' => 1];
         $cond = new condition($structure);
         $structure->type = 'recompletion';
         $this->assertEquals($structure, $cond->save());
+
+        $cond1 = new condition($structure);
+        $cond1->cm = '1';
+        $this->assertEquals($structure, $cond1->save());
+    }
+
+    /**
+     * Tests the save() function.
+     *
+     * @covers \availability_recompletion\condition::save()
+     */
+    public function test_save_not_equals() {
+        $structure = (object)['cm' => 1];
+        $cond = new condition($structure);
+        $cond->cm = 3;
+        $this->assertNotEquals($structure, $cond->save());
+    }
+
+    /**
+     * Tests the constructor error conditions.
+     *
+     * @covers \availability_recompletion\condition::__construct($structure)
+     */
+    public function test_constructor_exception() {
+        $msg = 'Coding error detected, it must be fixed by a programmer: Invalid course ID: null or non-integer value';
+
+        // No parameters.
+        $structure = new \stdClass();
+        try {
+            $cond = new condition($structure);
+            $this->fail();
+        } catch (\coding_exception $exception) {
+            $this->assertEquals(0, $exception->getCode());
+            $this->assertEquals($msg, $exception->getMessage());
+        }
+
+        // Invalid $cm.
+        $structure = (object)['cm' => 'a'];
+        try {
+            $cond = new condition($structure);
+        } catch (\coding_exception $exception) {
+            $this->assertEquals(0, $exception->getCode());
+            $this->assertEquals($msg, $exception->getMessage());
+        }
+    }
+
+    /**
+     * Tests the constructor and the string conversion feature (intended for debugging only).
+     *
+     * @covers \availability_recompletion\condition::__construct($structure)
+     * @covers \availability_recompletion\condition::get_debug_string()
+     */
+    public function test_constructor() {
+
+        $structure = (object)['cm' => 10242];
+        $cond = new condition($structure);
+        $this->assertEquals('{recompletion:10242 : NO}', (string)$cond);
+
+        $structure->cm = 4562;
+        $cond = new condition($structure);
+        $this->assertEquals('{recompletion:4562 : NO}', (string)$cond);
     }
 }
