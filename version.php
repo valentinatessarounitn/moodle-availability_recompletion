075bb9d3114fb3a2865aaf71116579e44e650902
valentinatessarounitn <valentina.tessaro.unitn@outlook.it>
2025-01-27 09:54:51 +0000

    Update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'availability_recompletion';
-$plugin->release = '0.1.1';
+$plugin->release = '1.0.0';
 $plugin->version = 2024111903;
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
